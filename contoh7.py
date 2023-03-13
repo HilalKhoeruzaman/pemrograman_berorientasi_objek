ee1eb6fbd9f1b222552b842a22df9261c04f85fa
HilalKhoeruzaman <93693029+HilalKhoeruzaman@users.noreply.github.com>
2023-03-13 11:53:26 +0700

    Add files via upload

---
+++ contoh7.py
@@ -0,0 +1,13 @@
+class Celcius :
+    @staticmethod
+    def to_fahrenheit(celcius):
+        return(celcius * 9/5) + 32
+    @staticmethod
+    def to_kelvin(celcius):
+        return celcius + 273.15
+    @staticmethod
+    def to_reamur(celcius):
+        return celcius * 4/5
+mycelcius = 80
+myfahrenheit = Celcius.to_fahrenheit(mycelcius)
+print(myfahrenheit)
