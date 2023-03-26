d5d440f17f460abf716dd4824ec7d073b0c45dfc
HilalKhoeruzaman <93693029+HilalKhoeruzaman@users.noreply.github.com>
2023-03-27 00:39:23 +0700

    Update hybird1.py

--- tugas2/hybird1.py
+++ tugas2/hybird1.py
@@ -1,49 +1,35 @@
-class Pemuda:
-    def __init__(self, name, age, address):
-        self.name = name
-        self.age = age
-        self.address = address
+class Hewan:
+    def __init__(self, nama, habitat):
+        self.nama = nama
+        self.habitat = habitat
+    def Makan(self):
+        print(f"{self.nama} sedang makan")
+class Karnivora(Hewan):
+    def __init__(self, nama, habitat, makanan):
+        super().__init__(self, nama, habitat)
+        self.makanan = makanan
+    def mencakar(self):
+        print(f"{self.nama} itu memangsa {self.makanan} untuk dimakan")
+class Kaki_Empat (Hewan):
+    def __init__(self, nama, habitat, suara):
+        super().__init__( nama, habitat)
+        self.suara = suara
+    def memangsa (self):
+        print(f"{self.nama} itu berlari mengejar {self.makanan} sambil mengaung {self.suara}")
 
-    def get_info(self):
-        print("Name:", self.name)
-        print("Age:", self.age)
-        print("Address:", self.address)
+class Harimau(Karnivora, Kaki_Empat):
+    def __init__(self, nama, habitat, makanan, suara):
+        Karnivora.__init__(self, nama, habitat, makanan)
+        Kaki_Empat.__init__(self, nama, habitat, suara)
+    def Mengaung(self):
+        print(f"{self.nama} mengaung sangat keras")
 
-# Single Inheritance
+harimauA = Harimau("Harimau Sumatra", "Hutan Sumatra", "Daging", "Rarrrrrr")
+harimauA.Mengaung()
+harimauA.memangsa()
+harimauA.mencakar()
+harimauA.Makan()
 
-class Mahasiswa(Pemuda):
 
-    def __init__(self, name, age, address, student_id):
-        super().__init__(name, age, address)
-        self.student_id = student_id
 
-    def get_info(self):
-        super().get_info()
-        print("Student ID:", self.student_id)
-
-# Single Inheritance
-
-class Santri(Pemuda):
-    def __init__(self, name, age, address, santri_id, pesantren):
-        super().__init__(name, age, address)
-        self.santri_id = santri_id
-        self.pesantren = pesantren
-
-    def get_info(self):
-        super().get_info()
-        print("santri ID:", self.santri_id)
-        print("Pesantren:", self.pesantren)
-
-# Multiple Inheritance
-
-class Programmer(Santri, Mahasiswa):
-    def __init__(self, name, age, address, santri_id, pesantren, student_id, program_language):
-        Santri.__init__(self, name, age, address, santri_id, pesantren)
-        Mahasiswa.__init__(self, name, age, address, student_id)
-        self.program_language = program_language
-
-    def get_info(self):
-        super().get_info()
-        print("Student ID:", self.student_id)
-        print("Bahasa Pemrogramman:", self.program_language)
-
+    
