ad37e79c089bea3b36a5016e9eed702d4fc14fc9
HilalKhoeruzaman <93693029+HilalKhoeruzaman@users.noreply.github.com>
2023-04-03 21:29:54 +0700

    Add files via upload

---
+++ tugas3/overload1.py
@@ -0,0 +1,5 @@
+print("Hai!" + " Seberapa Indonesiakah dirimu ?")
+print("Kalau benar kamu Orang Indonesia "+ " Aku mau nanya...")
+print("Ada berapa macam uang yang ada di Indonesia ? ")
+print(len([100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000, 100000]))
+print("Yups"+ " Betul Sekali!!" + " Anda Luar Biasa!!")
---
+++ tugas3/overload2.py
@@ -0,0 +1,10 @@
+print("Tebak-tebakan matematika")
+print("dari 1-10 mana angka yang terbesar?")
+print(max(1,2,3,4,5,6,7,8,9,10))
+print('Coba urutkan angka dibawah ini')
+print("100000,1000,50000,20000,10000,500,100,200,2000")
+print("Jawabannya adalah")
+a = [100000,1000,50000,20000,10000,500,100,200,2000]
+a.sort()
+print(a)
+print('Pintar sekali!!!')
