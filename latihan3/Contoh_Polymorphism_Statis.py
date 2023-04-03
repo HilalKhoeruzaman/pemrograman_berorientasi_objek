75a03bab0a954961ed18092118e1fa4a9862af28
HilalKhoeruzaman <93693029+HilalKhoeruzaman@users.noreply.github.com>
2023-04-03 21:25:31 +0700

    Add files via upload

---
+++ latihan3/Contoh_Polymorphism_Statis.py
@@ -0,0 +1,30 @@
+# Contoh 1
+# Fungsi 'len' pada berbagai tipe data
+
+print(len("Hello")) 
+print(len([1, 2, 3])) 
+print(len((1, 2, 3))) 
+
+# Contoh 2
+# Penggunaan operator “+” pada berbagai tipe data:
+
+print(2 + 3) # output: 5
+print("Hello" + " World") # output: Hello World
+print([1, 2] + [3, 4]) # output: [1, 2, 3, 4]
+
+# Contoh 3
+# Penggunaan fungsi “max” pada berbagai tipe data:
+
+print(max(2, 5)) # output: 5
+print(max([1, 2, 3])) # output: 3
+print(max("Hello")) # output: "o"
+
+# Contoh 4
+#Penggunaan metode “sort” pada berbagai tipe data:
+
+a = [3, 1, 2]
+a.sort()
+print(a) # output: [1, 2, 3]
+b = ["c", "a", "b"]
+b.sort()
+print(b) # output: ["a", "b", "c"]
